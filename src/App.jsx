7f73f883b42011343b1fee9f8602675e87862011
Shawn Witemyre <switemyre@comcast.net>
2025-09-25 19:30:45 -0400

    added keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { languages } from "../languages";
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react");
 
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
   const languageElements = languages.map((lang) => {
     const styles = {
       backgroundColor: lang.backgroundColor,
@@ -19,6 +21,11 @@ export default function AssemblyEndgame() {
   const letterElements = currentWord
     .split("")
     .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+
   return (
     <main>
       <header>
@@ -34,6 +41,8 @@ export default function AssemblyEndgame() {
       </section>
       <section className="language-chips">{languageElements}</section>
       <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   );
 }
