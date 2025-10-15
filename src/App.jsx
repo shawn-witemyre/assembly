99e909c50134718780ee1c958c1e862699b081c5
Shawn Witemyre <switemyre@comcast.net>
2025-10-14 23:07:26 -0400

    displayed all missed letters when you lose

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,17 @@ export default function AssemblyEndgame() {
     );
   });
 
-  const letterElements = currentWord
-    .split("")
-    .map((letter, index) => (
-      <span key={index}>
-        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+  const letterElements = currentWord.split("").map((letter, index) => {
+    const shouldRevealLetter = isGameLost || guessedLetters.includes(letter);
+    const letterClassName = clsx(
+      isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+    );
+    return (
+      <span key={index} className={letterClassName}>
+        {shouldRevealLetter ? letter.toUpperCase() : ""}
       </span>
-    ));
+    );
+  });
 
   function startNewGame() {
     setCurrentWord(getRandomWord());
